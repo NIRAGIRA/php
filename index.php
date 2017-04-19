86c64137a7eada130b6d0391d36bdedff5c92c21
NIRAGIRA <lniragira@bancobu.com>
2017-04-19 15:00:01 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> REPORTING BRB</title>
+	</head>
+	<body>
+		<h1>ETAT  DE RISQUES</h1>
+		
+		 <?php
+		 /*
+			$timestampDernierMois=time()-2592000;
+			echo date('d/m/Y',$timestampDernierMois),"\t\n\n";
+			echo date('d/m/Y',strtotime("+2 day")), "\n";
+			*/
+			echo 'Bonjour Ladislas<br/><br/>';
+		?> 
+		<form method="POST" action="#">
+		<label for="nom"> Nom</label><input type="text" name="nom" />
+		 <input type="submit" value="Envoyer" />
+		</form>
+		
+	</body>
+</html>
