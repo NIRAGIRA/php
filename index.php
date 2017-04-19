1b722262944c8811cffa2017152adebcda8889b0
NIRAGIRA <lniragira@bancobu.com>
2017-04-19 15:53:47 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -7,17 +7,18 @@
 		<h1>ETAT  DE RISQUES</h1>
 		
 		 <?php
-		 /*
+		 /* https://github.com/NIRAGIRA/php/upload/master
 			$timestampDernierMois=time()-2592000;
 			echo date('d/m/Y',$timestampDernierMois),"\t\n\n";
 			echo date('d/m/Y',strtotime("+2 day")), "\n";
 			*/
-			echo 'Bonjour Ladislas<br/><br/>';
+			$localtime = localtime();
+			$seconde =  $localtime[0];
+			$minute =  $localtime[1];
+			$heure =  $localtime[2];
+			echo $heure .'h'.$minute.'min'.$seconde;
+			
 		?> 
-		<form method="POST" action="#">
-		<label for="nom"> Nom</label><input type="text" name="nom" />
-		 <input type="submit" value="Envoyer" />
-		</form>
-		
+		<p> Aujourd'hui nous sommes <?php echo date('d/m/Y H:i:s')?></p>
 	</body>
 </html>
